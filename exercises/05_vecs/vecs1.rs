67244dc14bebf974811358f60a75db104d484586
bizmythy <andrew.p.council@gmail.com>
2026-01-05 23:11:15 -0500

    work from the plane

---
+++ exercises/05_vecs/vecs1.rs
@@ -0,0 +1,25 @@
+fn array_and_vec() -> ([i32; 4], Vec<i32>) {
+    let a = [10, 20, 30, 40]; // Array
+
+    // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
+    // Use the vector macro.
+    // idk how to use the macro, but this works fine. macro is better for doing at compile time, but idrc
+    let v: Vec<i32> = a.into();
+
+    (a, v)
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_array_and_vec_similarity() {
+        let (a, v) = array_and_vec();
+        assert_eq!(a, *v);
+    }
+}
