fcee64ae80b655a2916fb9787d735ee99f2000c8
bizmythy <andrew.p.council@gmail.com>
2026-01-05 23:14:21 -0500

    files from template, used to be in above dir

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+      fenix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        fenix-toolchain =
+          with fenix.packages.${system};
+          combine [
+            stable.toolchain
+            targets.wasm32-unknown-unknown.stable.rust-std
+          ];
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (aspellWithDicts (ps: with ps; [ en ]))
+            # keep-sorted start
+            fenix-toolchain
+            miniserve
+            nodejs
+            nushell
+            rustlings
+            rustup
+            wasm-pack
+            pnpm
+            # keep-sorted end
+          ];
+        };
+
+        formatter = treefmtEval.config.build.wrapper;
+        checks.formatting = treefmtEval.config.build.check self;
+      }
+    );
+}
---
+++ treefmt.nix
@@ -0,0 +1,29 @@
+{
+  ...
+}:
+{
+  projectRootFile = "flake.nix";
+
+  programs = {
+    nixfmt.enable = true;
+    jsonfmt.enable = true;
+    shellcheck.enable = true;
+    gofmt.enable = true;
+    ruff.enable = true;
+    yamlfmt.enable = true;
+    toml-sort.enable = true;
+    dos2unix.enable = true;
+    keep-sorted.enable = true;
+    rustfmt.enable = true;
+    # buggy as of right now
+    # nufmt.enable = true;
+  };
+
+  settings = {
+    # files to exlude from all formatting
+    excludes = [ ];
+    formatter = {
+      # formatter-specific settings
+    };
+  };
+}
